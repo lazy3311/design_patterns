181dc4abe4764ed676ae289764dca1160e60a027
Lazy <santosh3311@gmail.com>
2020-09-06 11:56:44 +0530

    facade

--- facade/main.cpp
+++ facade/main.cpp
@@ -2,10 +2,10 @@
 
 int main(int argc, char** argv) {
     std::cout << "-------------------------------------------------------------------" << std::endl;
-    std::cout << "                  PROXY PATTERN" << std::endl;
+    std::cout << "                  FACADE PATTERN" << std::endl;
     std::cout << "-------------------------------------------------------------------" << std::endl;
-    std::cout << "Proxy Design Pattern means ‘in place of’, representing’ or ‘in place of’ or \\
-    \n\r‘on behalf of’ something, to restrict the access to resources." << std::endl;
+    std::cout << "A facade is an object that serves as a front-facing interface\\
+    \n\rmasking more complex underlying or structural code." << std::endl;
     std::cout << "-------------------------------------------------------------------" << std::endl;
 
     return 0;
